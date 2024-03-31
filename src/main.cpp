599dbe7459c6cbe249d276d0fae9148e0a834811
Aryaman <aryaman@Aryamans-MacBook-Pro.local>
2024-03-31 15:26:19 -0500

    I am adding another line from vs code so that i can learn how to use it with vs code

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,4 @@
 
 std::cout<<"Hello this is my practice repo so that I can understand git"<<std::endl;
 
+cout<<"This is another message that is being added so that i can see how this will work woth VS code"<<std::endl;
