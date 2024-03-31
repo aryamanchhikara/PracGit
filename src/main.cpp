8309a964165a86d9e71fed5a33b650162d97b482
Aryaman <aryaman@Aryamans-MacBook-Pro.local>
2024-03-31 15:35:10 -0500

    This is a commit coming bron newbranch that needs to be merged

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,6 @@
 
 std::cout<<"Hello this is my practice repo so that I can understand git"<<std::endl;
 
-cout<<"This is another message that is being added so that i can see how this will work woth VS code"<<std::endl;
+std::cout<<"This is another message that is being added so that i can see how this will work woth VS code"<<std::endl;
+
+std::cout<<"This is a feature added in another branch"<<std::endl;
